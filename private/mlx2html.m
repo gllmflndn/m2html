e77893f6d1c8831f13c2b5089b3b3250825ae4cb
Guillaume <gllmflndn@users.noreply.github.com>
2020-05-04 09:43:35 +0100

    *.mlx file parser

---
+++ private/mlx2html.m
@@ -0,0 +1,142 @@
+function mlx2html(mlxfilename, htmlfilename)
+% Convert Live Script (*.mlx) file to HTML
+
+% Copyright (C) 2020 Guillaume Flandin <Guillaume@artefact.tk>
+
+%-Input arguments
+%--------------------------------------------------------------------------
+if nargin < 2
+    htmlfilename = [mlxfilename(1:end-3) 'html'];
+end
+
+%-Parse MLX file
+%--------------------------------------------------------------------------
+[mlx, rels] = mlxparse(mlxfilename);
+
+%-Get title
+%--------------------------------------------------------------------------
+i = find(arrayfun(@(x)strcmp(x.properties.style,'title'),mlx));
+if numel(i) > 0
+    title = mlx(i(1)).content.string;
+else
+    title = mlxfilename;
+end
+
+%-Export MLX file as HTML.
+%--------------------------------------------------------------------------
+fid = fopen(htmlfilename,'wt');
+if fid == -1
+    error('Cannot open HTML file for writing');
+end
+fprintf(fid,'<!DOCTYPE html>\n');
+fprintf(fid,'<html>\n');
+fprintf(fid,'<head>\n');
+fprintf(fid,'  <title>%s</title>\n',title);
+fprintf(fid,'  <meta charset="utf-8">\n');
+fprintf(fid,'  <meta name="viewport" content="width=device-width, initial-scale=1">\n');
+fprintf(fid,'  <meta name="description" content="%s">\n',title);
+fprintf(fid,'  <meta name="generator" content="m2html &copy; 2003-2020 https://www.artefact.tk">\n');
+fprintf(fid,'  <style>\n');
+fprintf(fid,'    body {background-color: white;}\n');
+fprintf(fid,'    h1,h2,p,ol,ul,pre {color: black;}\n');
+fprintf(fid,'    pre {background-color: #EEE; padding: 1em;}\n');
+fprintf(fid,'  </style>\n');
+fprintf(fid,'  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">\n');
+fprintf(fid,'  <script defer src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js" integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz" crossorigin="anonymous"></script>\n');
+fprintf(fid,'  <script defer src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"\n');
+fprintf(fid,'      onload="renderMathInElement(document.body);"></script>\n');
+fprintf(fid,'</head>\n');
+fprintf(fid,'<body>\n');
+list = false;
+for i=1:numel(mlx)
+    switch mlx(i).properties.style
+        case 'title'
+            fmt = '<h1>%s</h1>\n';
+        case 'heading'
+            fmt = '<h2>%s</h2>\n';
+        case 'heading2'
+            fmt = '<h3>%s</h3>\n';
+        case 'heading3'
+            fmt = '<h4>%s</h4>\n';
+        case 'section'
+            fmt = '<hr>%s\n';
+        case 'code'
+            fmt = '<pre>%s</pre>\n'; % %TODO% syntax highlighting https://highlightjs.org/
+        case 'text'
+            fmt = '<p>%s</p>\n';
+        case 'ListParagraph'
+            if mlx(i).properties.list == 1
+                pre = '<ul>'; post = '</ul>';
+            else
+                pre = '<ol>'; post = '</ol>';
+            end
+            if list, pre = ''; else, list = true; end
+            if i < numel(mlx) && strcmp(mlx(i+1).properties.style,'ListParagraph')
+                post = '';
+            else
+                list = false;
+            end
+            fmt = [pre '<li>%s</li>' post '\n'];
+        case {'CodeExampleLine','codeexample_matlab','codeexample_plain'}
+            fmt = '<pre>%s</pre>\n';
+        case 'LiveAppLine'
+            fmt = ''; % do not display
+        case {'TOCHeading','TOC1'}
+            fmt = ''; % do not display
+        otherwise
+            fmt = ''; % do not display
+    end
+    align = mlx(i).properties.align;
+    if ~strcmp(align,'left')
+        fmt = ['<div style="text-align:' align ';">' fmt(1:end-2) '</div>\n'];
+    end
+    bookmark = mlx(i).properties.bookmark;
+    if ~isempty(bookmark)
+        fmt = ['<a name="' bookmark '">' fmt(1:end-2) '</a>\n'];
+    end
+    str = '';
+    for j=1:numel(mlx(i).content)
+        s = mlx(i).content(j).string; % %TODO% escape HTML characters
+        if ~isempty(mlx(i).content(j).style.url)
+            s = ['<a href="' mlx(i).content(j).style.url '">' s '</a>'];
+        end
+        if ~isempty(mlx(i).content(j).style.anchor)
+            s = ['<a href="#' strrep(mlx(i).content(j).style.anchor,'internal:','') '">' s '</a>'];
+        end
+        if strcmp(mlx(i).content(j).style.type,'equation')
+            if mlx(i).content(j).style.displayStyle  % https://katex.org/
+                s = ['\[' s '\]'];
+            else
+                s = ['\(' s '\)'];
+            end
+        end
+        if strcmp(mlx(i).content(j).style.type,'image')
+            rId = mlx(i).content(j).style.relationshipId;
+            try
+                [~,~,ext] = fileparts(rels.(rId).target);
+                s = sprintf('<img src="data:image/%s;base64,%s" width="%s" height="%s">',...
+                    ext(2:end),base64encode(rels.(rId).raw),...
+                    mlx(i).content(j).style.width,mlx(i).content(j).style.height);
+            catch
+                s = ['<!-- ' rId ' -->'];
+            end
+        end
+        if mlx(i).content(j).style.bold
+            s = ['<strong>' s '</strong>'];
+        end
+        if mlx(i).content(j).style.italic
+            s = ['<em>' s '</em>'];
+        end
+        if mlx(i).content(j).style.underline
+            s = ['<u>' s '</u>'];
+        end
+        if mlx(i).content(j).style.monospace
+            s = ['<code>' s '</code>'];
+        end
+        str = [str ' ' s];
+    end
+    fprintf(fid,fmt,str(2:end));
+end
+fprintf(fid,'</body>\n');
+fprintf(fid,'</html>\n');
+fclose(fid);
---
+++ private/mlxparse.m
@@ -0,0 +1,174 @@
+function [mlx, rels] = mlxparser(filename)
+% Parse Live Script (*.mlx) files
+%
+% https://www.mathworks.com/help/matlab/matlab_prog/live-script-file-format.html
+
+% Copyright (C) 2020 Guillaume Flandin <Guillaume@artefact.tk>
+
+mlx = struct('properties',{},'content',{});
+
+%-Extract files from archive
+%==========================================================================
+tmpdir = tempname;
+[sts,msg] = mkdir(tmpdir);
+if ~sts, error('Cannot create temporary directory.'); end
+try
+    unzip(filename,tmpdir);
+catch
+    error('Cannot open MLX file "%s".',filename);
+end
+ocn = onCleanup(@()rmdir(tmpdir,'s'));
+
+%-Parse document
+%==========================================================================
+try
+    % Require https://github.com/gllmflndn/gifti/tree/master/%40gifti/private/xml_parser.mex*
+    xml = xml_parser(fullfile(tmpdir,'matlab','document.xml'));
+catch
+    error('Cannot parse the MATLAB document.');
+end
+
+%-Get all Paragraphs w:document>w:body>w:p
+%--------------------------------------------------------------------------
+p = find(arrayfun(@(x)strcmp(x.value,'w:p'),xml));
+
+%-Extract text and style for each paragraph
+%--------------------------------------------------------------------------
+for i=1:numel(p)
+    c = xml(p(i)).children;
+    props = struct('style','','align','','list',0,'bookmark','');
+    content = struct('string',{},'style',{});
+    for j=1:numel(c)
+        if strcmp(xml(c(j)).value,'mc:AlternateContent')
+            % mc:AlternateContent>mc:Choice/mc:Fallback>* - take first...
+            cr = xml(c(j)).children;
+            c(j) = xml(cr(1)).children;
+        end
+        switch xml(c(j)).value
+            case 'w:pPr' % ParagraphProperties
+                cr = xml(c(j)).children;
+                for k=1:numel(cr)
+                    switch xml(cr(k)).value
+                        case 'w:pStyle' % Referenced Paragraph Style 
+                        	props.style = attr(xml(cr(k)),'w:val');
+                        case 'w:jc'     % Paragraph Alignment
+                            props.align = attr(xml(cr(k)),'w:val');
+                        case 'w:numPr'  % Numbering Definition Instance Reference
+                            % ListParagraph: 1 -> unordered, 2 -> ordered
+                            props.list = str2num(attr(xml(xml(cr(k)).children),'w:val'));
+                        case 'w:sectPr' % Document Final Section Properties
+                            props.style = 'section';
+                        otherwise
+                            fprintf('Ignored "%s" property\n',xml(cr(k)).value');
+                    end
+                end
+            case 'w:r' % Run
+                [str, style] = runcontent(xml,c(j));
+                content = [content, struct('string',{str},'style',{style})];
+            case 'w:customXml'
+                type = attr(xml(c(j)),'w:element'); % image, equation, livecontrol
+                % -> w:r>w:t
+                k = xml(c(j)).children;
+                k = k(arrayfun(@(x)strcmp(x.value,'w:r'),xml(k)));
+                [str, style] = runcontent(xml,k);
+                style.type = type;
+                % -> w:customXmlPr>w:attr : displayStyle = false
+                % -> w:customXmlPr>w:attr : height, width, relationshipId
+                k = xml(c(j)).children;
+                k = k(arrayfun(@(x)strcmp(x.value,'w:customXmlPr'),xml(k)));
+                if ~isempty(k)
+                    k = xml(k).children;
+                    for l=1:numel(k)
+                        n = attr(xml(k(l)),'w:name');
+                        v = attr(xml(k(l)),'w:val');
+                        if strcmpi(v,'true'), v = true; end
+                        if strcmpi(v,'false'), v = false; end
+                        style.(n) = v;
+                    end
+                end
+                content = [content, struct('string',{str},'style',{style})];
+            case 'w:hyperlink'
+                [str, style] = runcontent(xml,xml(c(j)).children);
+                style.url = attr(xml(c(j)),'w:docLocation');
+                style.anchor = attr(xml(c(j)),'w:anchor');
+                content = [content, struct('string',{str},'style',{style})];
+            case 'w:bookmarkStart'
+                name = attr(xml(c(j)),'w:name');
+                id = attr(xml(c(j)),'w:id');
+                props.bookmark = id;
+            case 'w:bookmarkEnd'
+                id = attr(xml(c(j)),'w:id');
+                props.bookmark = id;
+            otherwise
+               fprintf('Ignored "%s" element\n',xml(c(j)).value);
+       end
+    end
+    mlx(i) = struct('properties',props,'content',content);
+end
+
+%-Parse relations
+%==========================================================================
+rels = struct;
+if nargout > 1
+    try
+        rel = xml_parser(fullfile(tmpdir,'matlab','_rels','document.xml.rels'));
+        r = find(arrayfun(@(x)strcmp(x.value,'Relationship'),rel));
+        for i=1:numel(r)
+            rels.(attr(rel(r(i)),'Id')).target = attr(rel(r(i)),'Target');
+            rels.(attr(rel(r(i)),'Id')).type = attr(rel(r(i)),'Type');
+            fid = fopen(fullfile(tmpdir,'matlab',attr(rel(r(i)),'Target')),'rb');
+            rels.(attr(rel(r(i)),'Id')).raw = fread(fid,Inf,'*uchar');
+            fclose(fid);
+        end
+    end
+end
+
+
+%==========================================================================
+%-Get XML element attributes
+%==========================================================================
+function value = attr(element,key)
+% Return value of a given attribute key
+value = '';
+for i=1:numel(element.attributes)
+    if strcmp(element.attributes(i).key,key)
+        value = element.attributes(i).value;
+        return;
+    end
+end
+
+%==========================================================================
+%-Get Run content
+%==========================================================================
+function [str,style] = runcontent(xml,uid)
+% Return content of w:r>w:t and w:r>w:rPr
+str = '';
+style = struct('type','text','bold',false, 'italic',false, 'underline',false,...
+    'monospace',false, 'url','','anchor','');
+if nargin < 2 || isempty(uid), return; end
+cr = [xml(xml(uid).children).children];
+for k=1:numel(cr)
+    switch xml(cr(k)).type
+        case 'chardata' % Text
+            str = xml(cr(k)).value;
+        case 'element' % RunProperties
+            switch xml(cr(k)).value
+                case 'w:rFonts'
+                    font = attr(xml(cr(k)),'w:cs');
+                    if strcmp(font,'monospace')
+                        style.monospace = true;
+                    else
+                        fprintf('Ignored "%s" font\n',font);
+                    end
+                case 'w:b'
+                    style.bold = true;
+                case 'w:i'
+                    style.italic = true;
+                case 'w:u'
+                    style.underline = true;
+                otherwise
+                    fprintf('Ignored "%s" style\n',xml(cr(k)).value);
+            end
+    end
+end
+            
